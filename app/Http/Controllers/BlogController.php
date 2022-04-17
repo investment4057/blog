7a72318347d5a7f192aad61d62630aafe425940b
investment4057 <investment4057@gmail.com>
2022-04-18 01:08:33 +0900

    MVCでブログ詳細画面を表示

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -15,4 +15,17 @@ class BlogController extends Controller
 
         return view('blog.list', ['blogs' => $blogs]);
     }
+
+    // ブログ詳細を表示
+    public function showDetail($id) {
+        $blog = Blog::find($id);
+
+        // IDが見つからなかった（$blogがnull）場合の分岐
+        if (is_null($blog)) {
+            \Session::flash('err_msg', 'データがありません。');
+            return redirect(route('blogs'));
+        }
+
+        return view('blog.detail', ['blog' => $blog]);
+    }
 }
